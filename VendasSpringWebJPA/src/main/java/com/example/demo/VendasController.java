7ac781424cd8b9543fd4d2ff10590d3807466c72
eschmitz <eschmitz@CIWRKST06E0D03.cipucrs.pucrs.br>
2017-08-25 14:02:27 -0300

    tabela de produtos pronta

--- VendasSpringWebJPA/src/main/java/com/example/demo/VendasController.java
+++ VendasSpringWebJPA/src/main/java/com/example/demo/VendasController.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@RestController
+@Controller
 public class VendasController {
 	private ProdutoRepositorioV2 pr;
 	private VendaRepositorioV2 vr;
@@ -26,8 +30,10 @@ public class VendasController {
 	}
 	
 	@GetMapping("/produtos")
-	public Iterable<Produto> consultarProdutos() {
-		return pr.findAll();
+	public String consultarProdutos(Model model) {
+		
+		model.addAttribute("pr",pr.findAll());
+		return "produtos";
 	}
 		
 	@GetMapping("/vendas")
